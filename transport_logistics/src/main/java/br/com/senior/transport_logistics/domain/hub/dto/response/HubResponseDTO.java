b14d1df5fd26494dbd12a260106ecb37d0c1515a
martin.garrote <martin.garrote@senior.com.br>
2025-08-06 15:42:20 -0300

    atualizando hub response dto

--- transport_logistics/src/main/java/br/com/senior/transport_logistics/domain/hub/dto/response/HubResponseDTO.java
+++ transport_logistics/src/main/java/br/com/senior/transport_logistics/domain/hub/dto/response/HubResponseDTO.java
@@ -1,5 +1,6 @@
 package br.com.senior.transport_logistics.domain.hub.dto.response;
 
+import br.com.senior.transport_logistics.domain.hub.HubEntity;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 
@@ -20,22 +21,33 @@ public record HubResponseDTO(
         String cep
 ) {
 
-    public HubResponseDTO(Long id, String name, String cnpj, String cep, String number) {
-        this(id, name, cnpj, null, number, null, null, null, null, null, null, cep);
+    public static HubResponseDTO basic(HubEntity entity) {
+        return HubResponseDTO
+                .builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .cnpj(entity.getCnpj())
+                .number(entity.getNumber())
+                .cep(entity.getCep())
+                .build();
     }
 
-    public HubResponseDTO(Long id, String name, String cnpj, String street, String number, String district, String city, String state, String country, Double latitude, Double longitude, String cep) {
-        this.id = id;
-        this.name = name;
-        this.cnpj = cnpj;
-        this.street = street;
-        this.number = number;
-        this.district = district;
-        this.city = city;
-        this.state = state;
-        this.country = country;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.cep = cep;
+    public static HubResponseDTO detailed(HubEntity entity) {
+        return HubResponseDTO
+                .builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .cnpj(entity.getCnpj())
+                .street(entity.getStreet())
+                .number(entity.getNumber())
+                .district(entity.getDistrict())
+                .city(entity.getCity())
+                .state(entity.getState())
+                .country(entity.getCountry())
+                .latitude(entity.getLatitude())
+                .longitude(entity.getLongitude())
+                .cep(entity.getCep())
+                .build();
+
     }
 }
