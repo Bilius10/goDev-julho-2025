24b3f95792f7af36d25dbb1099e79db86311b3a5
martin.garrote <martin.garrote@senior.com.br>
2025-08-06 16:48:32 -0300

    implementando entidade caminhão e respectivos enums

---
+++ transport_logistics/src/main/java/br/com/senior/transport_logistics/domain/truck/enums/TruckStatus.java
@@ -0,0 +1,18 @@
+package br.com.senior.transport_logistics.domain.truck.enums;
+
+public enum TruckStatus {
+    AVAILABLE("Disponível"),
+    ON_TRANSIT("Em Trânsito"),
+    MAINTENANCE("Manutenção"),
+    OUT_OF_ORDER("Fora de Serviço");
+
+    private final String description;
+
+    TruckStatus(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
