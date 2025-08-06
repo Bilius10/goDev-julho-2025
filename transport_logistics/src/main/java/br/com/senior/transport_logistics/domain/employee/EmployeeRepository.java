ab001bebaa1ca37a549aca2532d73d22383dbff3
João Vitor <jvdrdo@gmail.com>
2025-08-06 16:24:07 -0300

    Update EmployeeRepository.java
    inserindo existsby oara cnh, cpf e email

--- transport_logistics/src/main/java/br/com/senior/transport_logistics/domain/employee/EmployeeRepository.java
+++ transport_logistics/src/main/java/br/com/senior/transport_logistics/domain/employee/EmployeeRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
     boolean existsByNameIgnoreCase(String name);
+    boolean existsByCnh(String cnh);
+    boolean existsByCpf(String cpf);
+    boolean existsByEmail(String email);
 }
