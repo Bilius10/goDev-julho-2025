b6972be0fbc2c299a3fa607e834fc510ac7c0d2d
João Vitor da Rosa de Oliveira <joao.deoliveira@senior.com.br>
2025-08-06 09:32:23 -0300

    Project structure

---
+++ transport_logistics/src/test/java/br/com/senior/transport_logistics/TransportLogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senior.transport_logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportLogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
