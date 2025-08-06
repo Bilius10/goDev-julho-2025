f7eeb20db6c17a69a4ad0cbce5b51d30cc612518
Nicole Senior <nicolesypriany@gmail.com>
2025-08-06 16:16:05 -0300

    corrigindo EmployeeResponseDTO

--- transport_logistics/src/main/java/br/com/senior/transport_logistics/domain/employee/dto/response/EmployeeResponseDTO.java
+++ transport_logistics/src/main/java/br/com/senior/transport_logistics/domain/employee/dto/response/EmployeeResponseDTO.java
@@ -1,9 +1,13 @@
 package br.com.senior.transport_logistics.domain.employee.dto.response;
 
+import br.com.senior.transport_logistics.domain.employee.EmployeeEntity;
 import br.com.senior.transport_logistics.domain.employee.enums.Role;
+import br.com.senior.transport_logistics.domain.hub.HubEntity;
 import br.com.senior.transport_logistics.domain.hub.dto.response.HubResponseDTO;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
 
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record EmployeeResponseDTO(
         long id,
@@ -15,4 +19,17 @@ public record EmployeeResponseDTO(
         Role role,
         HubResponseDTO hub
 ) {
+
+    public static EmployeeResponseDTO basic(EmployeeEntity entity, HubEntity hub) {
+        return EmployeeResponseDTO
+                .builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .cnh(entity.getCnh())
+                .cpf(entity.getCpf())
+                .email(entity.getEmail())
+                .role(entity.getRole())
+                .hub(HubResponseDTO.basic(hub))
+                .build();
+    }
 }
