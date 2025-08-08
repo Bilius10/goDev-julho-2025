9eb39110247807792d2a134bbba48944debb8131
martin.garrote <martin.garrote@senior.com.br>
2025-08-08 13:49:05 -0300

    adicionando mensagens para employees

--- transport_logistics/src/main/java/br/com/senior/transport_logistics/infrastructure/exception/ExceptionMessages.java
+++ transport_logistics/src/main/java/br/com/senior/transport_logistics/infrastructure/exception/ExceptionMessages.java
@@ -3,9 +3,16 @@ package br.com.senior.transport_logistics.infrastructure.exception;
 public enum ExceptionMessages {
 
     HUB_NOT_FOUND_BY_ID("Filial com o campo 'id' igual a '%s' não foi encontrado."),
-    HUB_NAME_IN_USE("O campo nome com valor '%s' já está em uso para Filial."),
-    HUB_CNPJ_IN_USE("O campo CNPJ com valor '%s' já está em uso para Filial."),
-    HUB_ALREADY_EXISTS_IN_CITY("A cidade %s já possui uma filial.");
+    HUB_NAME_IN_USE("O campo nome com valor '%s' já está em uso por outra filial."),
+    HUB_CNPJ_IN_USE("O campo CNPJ com valor '%s' já está em uso por outra filial."),
+    HUB_ALREADY_EXISTS_IN_CITY("A cidade %s já possui uma filial."),
+
+    EMPLOYEE_NOT_FOUND_BY_ID("Funcionário com o campo 'id' igual a '%s' não foi encontrado"),
+    EMPLOYEE_EMAIL_IN_USE("O campo email com valor '%s' já está em uso por outro funcionário"),
+    EMPLOYEE_CPF_IN_USE("O campo CPF com valor '%s' já está em uso por outro funcionário."),
+    EMPLOYEE_CNH_IN_USE("O campo CNH com valor '%s' já está em uso por outro funcionário."),
+
+    ERROR("ERROR");
 
     private final String template;
 
