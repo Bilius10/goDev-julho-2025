e79f2bb7fcad29e1884a622f7a139fd134edac69
martin.garrote <martin.garrote@senior.com.br>
2025-08-08 11:33:10 -0300

    adaptando exceções customizadas ao novo padrão com enum

--- transport_logistics/src/main/java/br/com/senior/transport_logistics/infrastructure/exception/common/FieldAlreadyExistsException.java
+++ transport_logistics/src/main/java/br/com/senior/transport_logistics/infrastructure/exception/common/FieldAlreadyExistsException.java
@@ -3,8 +3,8 @@ package br.com.senior.transport_logistics.infrastructure.exception.common;
 import org.springframework.http.HttpStatus;
 
 public class FieldAlreadyExistsException extends BaseException {
-    public FieldAlreadyExistsException(String entity, String field, String value) {
-        super(String.format("O campo %s com valor '%s' já está em uso para %s.", field, value, entity));
+    public FieldAlreadyExistsException(String message) {
+        super(message);
     }
 
 
--- transport_logistics/src/main/java/br/com/senior/transport_logistics/infrastructure/exception/common/ResourceNotFoundException.java
+++ transport_logistics/src/main/java/br/com/senior/transport_logistics/infrastructure/exception/common/ResourceNotFoundException.java
@@ -3,8 +3,8 @@ package br.com.senior.transport_logistics.infrastructure.exception.common;
 import org.springframework.http.HttpStatus;
 
 public class ResourceNotFoundException extends BaseException {
-    public ResourceNotFoundException(String entity, String field, String value) {
-        super(String.format("%s com o campo '%s' igual a '%s' não foi encontrado.", entity, field, value));
+    public ResourceNotFoundException(String message) {
+        super(message);
     }
 
     @Override
