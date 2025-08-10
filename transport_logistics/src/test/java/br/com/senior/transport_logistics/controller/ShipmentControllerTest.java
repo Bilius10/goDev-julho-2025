69d81e43720f04445c17cbfb5ad75493b827260a
Bilius10 <jvdrdo@gmail.com>
2025-08-10 15:09:35 -0300

    iniciando os testes

---
+++ transport_logistics/src/test/java/br/com/senior/transport_logistics/controller/ShipmentControllerTest.java
@@ -0,0 +1,24 @@
+package br.com.senior.transport_logistics.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShipmentControllerTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ transport_logistics/src/test/java/br/com/senior/transport_logistics/domain/shipment/ShipmentServiceTest.java
@@ -0,0 +1,28 @@
+package br.com.senior.transport_logistics.domain.shipment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShipmentServiceTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
