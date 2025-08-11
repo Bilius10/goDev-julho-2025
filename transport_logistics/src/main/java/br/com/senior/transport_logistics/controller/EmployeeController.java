cd8ba0dc3beb71e845437589fda9d0b4055cefa2
martin.garrote <martin.garrote@senior.com.br>
2025-08-11 15:58:40 -0300

    adicionando anotações para AuthController e EmployeeController

--- transport_logistics/src/main/java/br/com/senior/transport_logistics/controller/EmployeeController.java
+++ transport_logistics/src/main/java/br/com/senior/transport_logistics/controller/EmployeeController.java
@@ -1,29 +1,36 @@
 package br.com.senior.transport_logistics.controller;
 
-import br.com.senior.transport_logistics.domain.employee.EmployeeEntity;
 import br.com.senior.transport_logistics.domain.employee.EmployeeService;
-import br.com.senior.transport_logistics.domain.employee.dto.request.EmployeePasswordUpdateDTO;
 import br.com.senior.transport_logistics.domain.employee.dto.request.EmployeeUpdateRequestDTO;
 import br.com.senior.transport_logistics.domain.employee.dto.response.EmployeeResponseDTO;
 import br.com.senior.transport_logistics.domain.employee.enums.Role;
 import br.com.senior.transport_logistics.infrastructure.dto.PageDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/employees")
+@SecurityRequirement(name = "bearer-key")
+@Tag(name = "EmployeeController", description = "Endpoints de listagem, atualização e remoção de funcionários")
 public class EmployeeController {
 
     private final EmployeeService service;
 
+    @Operation(summary = "Endpoint para listar funcionários com paginação")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listagem paginada dos funcionários")
+    })
     @GetMapping
     public ResponseEntity<PageDTO<EmployeeResponseDTO>> findAll(
             @RequestParam(defaultValue = "0", required = false) int page,
@@ -35,30 +42,38 @@ public class EmployeeController {
         return ResponseEntity.status(HttpStatus.OK).body(service.findAll(pageable));
     }
 
+    @Operation(summary = "Endpoint para atualizar um funcionário via ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Atualização realizada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados de entrada inválidos")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<EmployeeResponseDTO> update(
             @PathVariable Long id,
-            @RequestBody @Valid EmployeeUpdateRequestDTO employeeUpdateDTO){
+            @RequestBody @Valid EmployeeUpdateRequestDTO employeeUpdateDTO) {
 
         return ResponseEntity.status(HttpStatus.OK).body(service.update(id, employeeUpdateDTO));
     }
 
-    @PatchMapping("/password")
-    public ResponseEntity<EmployeeResponseDTO> updatePassword(@AuthenticationPrincipal EmployeeEntity employee,
-                                                              @RequestBody @Valid EmployeePasswordUpdateDTO employeePasswordUpdateDTO) {
-        service.updatePassword(employee, employeePasswordUpdateDTO);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
-
+    @Operation(summary = "Endpoint para trocar Role de funcionário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Senha atualizada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Funcionário não encontrado com o ID informado"),
+            @ApiResponse(responseCode = "400", description = "Role não informada ou inválida")
+    })
     @PatchMapping("/{id}/role")
     public ResponseEntity<EmployeeResponseDTO> updateRole(@PathVariable Long id, @RequestParam Role role) {
         service.updateRole(id, role);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @Operation(summary = "Endpoint para deletar funcionário (delete lógico)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Funcionário deletado com sucesso (lógico)"),
+            @ApiResponse(responseCode = "404", description = "Nenhum funcionário localizado com o ID informado")
+    })
     @DeleteMapping("/{id}")
-    @Transactional
-    public ResponseEntity<EmployeeResponseDTO> delete(@PathVariable Long id){
+    public ResponseEntity<EmployeeResponseDTO> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
