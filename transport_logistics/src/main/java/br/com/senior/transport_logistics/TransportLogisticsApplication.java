7ed2ebaca700da03f9f81c96053834d60f6670fb
martin.garrote <martin.garrote@senior.com.br>
2025-08-12 10:42:09 -0300

    adicionando informações da documentação na config

--- transport_logistics/src/main/java/br/com/senior/transport_logistics/TransportLogisticsApplication.java
+++ transport_logistics/src/main/java/br/com/senior/transport_logistics/TransportLogisticsApplication.java
@@ -1,7 +1,5 @@
 package br.com.senior.transport_logistics;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -10,17 +8,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @SpringBootApplication
 @EnableScheduling
-@OpenAPIDefinition(
-		info = @Info(
-				title = "LogiTrack API",
-				version = "1.0",
-				description = "Documentação da API LogiTrack"
-		)
-)
 public class TransportLogisticsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TransportLogisticsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TransportLogisticsApplication.class, args);
+    }
 
 }
